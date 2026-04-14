f69c5a908c245fc2ef6d65c0227fff4bb994d1d3
Rafa C Maia <57460462+rafacmaia@users.noreply.github.com>
2026-04-14 16:00:40 -0400

    feat(ui): add mobile responsiveness to the Nav Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,22 +5,31 @@ export default function Header() {
   function activeStyle({ isActive }: { isActive: boolean }): string {
     return isActive
       ? 'underline decoration-wavy decoration-2 underline-offset-4 decoration-accent/80'
-      : 'opacity-90 hover:opacity-100 hover:scale-102 transition-all duration-200  hover:-translate-y-0.5'
+      : 'opacity-90 active:opacity-100 active:scale-96 hover:sm:opacity-100 hover:sm:scale-102 transition-all duration-200 hover:sm:-translate-y-0.5'
   }
 
   return (
-    <header className={'flex w-full px-6 py-3'}>
-      <nav className="ml-auto flex items-center gap-8 align-middle font-zain text-[20px] font-extrabold tracking-wide text-primary/95">
+    <header className={'flex w-full px-1 py-3 sm:px-6 sm:py-3'}>
+      <nav className="flex w-full items-center justify-around align-middle font-zain text-[20px] font-extrabold text-primary/95 sm:justify-end sm:gap-8 sm:tracking-wide">
         <NavLink to={'/brawl'} className={activeStyle}>
           Brawl Pit
         </NavLink>
         <NavLink to={'/leaderboard'} className={activeStyle}>
           Leaderboard
         </NavLink>
-        <NavLink to={'/manage'} className={activeStyle}>
+        <NavLink
+          to={'/manage'}
+          className={({ isActive }) => `${activeStyle({ isActive })} hidden sm:inline`}
+        >
           Manage the Pit
         </NavLink>
-        <div className={`translate-y-1`}>
+        <NavLink
+          to={'/manage'}
+          className={({ isActive }) => `${activeStyle({ isActive })} sm:hidden`}
+        >
+          Manage Pit
+        </NavLink>
+        <div className={`translate-y-0.5 sm:translate-y-0.75`}>
           <UserButton />
         </div>
       </nav>
