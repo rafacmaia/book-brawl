26135efa9dee792a8aaed6485e76e1f3e49fb6bd
Rafa C Maia <57460462+rafacmaia@users.noreply.github.com>
2026-04-13 22:16:01 -0400

    ui/ux(leaderboard): update table styling to match ManagePit

--- frontend/src/pages/Leaderboard.tsx
+++ frontend/src/pages/Leaderboard.tsx
@@ -2,7 +2,7 @@ import { useAuth } from '@clerk/react'
 import { useEffect, useState } from 'react'
 import { apiFetch } from '../api'
 import Placeholder from '../components/Placeholder'
-import { PageHeading } from '../components/PageHeading.tsx'
+import PageHeading from '../components/PageHeading'
 
 interface BookData {
   rank: string
@@ -47,6 +47,11 @@ export default function Leaderboard() {
       const rankingsData = await rankingsRes.json()
       const progressData = await progressRes.json()
 
+      if (progressData.book_count === 0) {
+        setError('No books in the Pit. Feed the Pit to start!')
+        return
+      }
+
       setRankings(rankingsData)
       setProgress(progressData.progress)
       setBookCount(progressData.book_count)
@@ -56,6 +61,10 @@ export default function Leaderboard() {
       setLoading(false)
     }
   }
+
+  const cellXPadding = 'px-2'
+  const thStyling = `pt-2 first:pl-5  pb-1 tracking-wide text-[20px] font-extrabold font-calistoga ${cellXPadding}`
+  const tdStyling = `py-1.5 first:pl-6 ${cellXPadding}`
 
   return (
     <main className="flex h-full min-h-0 grow flex-col items-center gap-8 overflow-y-auto p-4 text-primary/95">
@@ -68,28 +77,30 @@ export default function Leaderboard() {
         <>
           <h2
             className={
-              'mt-2 rounded-full bg-primary/90 px-10 py-2 text-center font-calistoga text-[22px] font-bold tracking-wide text-text shadow-2xl'
+              'mt-4 rounded-full bg-button px-10 py-2 text-center font-calistoga text-[22px] font-bold tracking-wide text-text shadow-2xl'
             }
           >
             {bookCount} Books<span className={'mx-6'}>•</span>
             {Math.round(progress * 100)}% Complete
           </h2>
-          <table className="mt-2 w-2/3 table-fixed border-collapse rounded-md bg-primary/90 text-text shadow-lg">
-            <thead className={'text-left text-[20px] text-accent'}>
-              <tr className={'border-b-3 border-accent'}>
-                <th className={`w-1/13 pt-2 pr-4 pb-1 pl-6 font-extrabold`}>#</th>
-                <th className={`w-6/13 px-4 pt-2 pb-1 font-extrabold`}>Title</th>
-                <th className={`w-4/13 px-4 pt-2 pb-1 font-extrabold`}>Author</th>
-                <th className={`w-2/13 pt-2 pr-6 pb-1 pl-4 font-extrabold`}>Accuracy</th>
+          <table className="w-3/5 table-fixed border-collapse rounded-md bg-button text-text shadow-lg">
+            <thead className={'text-left'}>
+              <tr className={'border-b-3 border-red-800'}>
+                <th className={`w-[10%] ${thStyling}`}>Rank</th>
+                <th className={`w-[45%] ${thStyling}`}>Title</th>
+                <th className={`w-[30%] ${thStyling}`}>Author</th>
+                <th className={`w-[15%] ${thStyling}`}>Accuracy</th>
               </tr>
             </thead>
-            <tbody className={'text-[18px]'}>
+            <tbody className={'text-[18px] opacity-95'}>
               {rankings.map((book) => (
-                <tr key={book.rank} className={'border-b-2 border-background'}>
-                  <td className={`py-1 pr-4 pl-7 font-extrabold text-accent`}>{book.rank}</td>
-                  <td className={`px-4 py-1 font-bold`}>{book.title}</td>
-                  <td className={`px-4 py-1`}>{book.author}</td>
-                  <td className={`py-1 pr-7 pl-4`}>{TIER_LABELS[book.accuracy_tier]}</td>
+                <tr key={book.rank} className={'border-b-2 border-red-800/80 last:border-none'}>
+                  <td className={`font-calistoga font-bold text-red-900/75 ${tdStyling}`}>
+                    {book.rank}
+                  </td>
+                  <td className={`font-bold ${tdStyling}`}>{book.title}</td>
+                  <td className={tdStyling}>{book.author}</td>
+                  <td className={tdStyling}>{TIER_LABELS[book.accuracy_tier]}</td>
                 </tr>
               ))}
             </tbody>
