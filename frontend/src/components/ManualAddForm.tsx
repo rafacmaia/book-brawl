5f3aa26b44b4878844b6c12b702505f908e8bc44
Rafa C Maia <57460462+rafacmaia@users.noreply.github.com>
2026-09-20 16:29:09 -0400

    feat(ui): disable manual input form while processing a new entry

--- frontend/src/components/ManualAddForm.tsx
+++ frontend/src/components/ManualAddForm.tsx
@@ -9,14 +9,14 @@ const styles = {
   default: {
     form: 'gap-3 flex w-full flex-col justify-between font-bold text-text sm:text-lg md:flex-row md:gap-0',
     input:
-      'rounded-md text-base border-b-3 font-calistoga border-primary/85 bg-blue-200 py-2.5 px-3 sm:p-2 sm:pl-3 shadow-lg',
+      'rounded-md text-base border-b-3 font-calistoga border-primary/85 placeholder-text/75 bg-blue-200 py-2.5 px-3 sm:p-2 sm:pl-3 shadow-lg disabled:opacity-65',
     button:
-      'text-lg cursor-pointer rounded-md border-b-3 border-primary/75 bg-accent/80 pt-2.25 pb-1.5 text-center font-zain font-extrabold text-primary/90 shadow-md transition-all hover:scale-102 hover:bg-accent/85 active:scale-97 active:bg-accent/70 md:w-[8%] md:bg-accent/75 md:pt-1.75 md:pb-0.75 md:text-xl',
+      'text-lg cursor-pointer rounded-md border-b-3 border-primary/70 bg-accent/80 pt-2.25 pb-1.5 text-center font-zain font-extrabold text-primary/90 shadow-md transition-all hover:scale-102 hover:bg-accent/85 active:scale-97 active:bg-accent/70 md:w-[8%] md:bg-accent/75 md:pt-1.75 md:pb-0.75 md:text-xl',
   },
   compact: {
     form: 'gap-2 flex w-full flex-col font-bold text-text md:gap-2.5',
     input:
-      'rounded-lg text-sm border-b-3 border-primary/85 bg-blue-200 py-2 px-3 shadow-lg md:text-base md:py-2.5 font-calistoga ',
+      'rounded-lg text-sm border-b-3 placeholder-text/70  border-primary/85 bg-blue-200 py-2 px-3 shadow-lg md:text-base md:py-2.5 font-calistoga disabled:opacity-65',
     button:
       'text-base cursor-pointer rounded-lg w-full border-b-3 border-primary/75 bg-accent/80 pt-2 pb-1.5 text-center font-zain font-extrabold text-primary/90 shadow-md transition-all hover:scale-102 hover:bg-accent/85 active:scale-97 active:bg-accent/70 md:bg-accent/75 md:text-xl md:pt-2.5',
   },
@@ -31,6 +31,7 @@ interface Props {
 
 export function ManualAddForm({ addState, addBook, onSuccess, variant = 'default' }: Props) {
   const style = styles[variant]
+  const isLoading = addState.type === 'loading'
 
   const titleRef = useRef<HTMLInputElement>(null)
 
@@ -64,31 +65,39 @@ export function ManualAddForm({ addState, addBook, onSuccess, variant = 'default
     <>
       <form className={style.form} onKeyDown={handleKeyDown} onSubmit={handleSubmit}>
         <input
-          aria-label="Enter book title"
-          type="text"
-          placeholder="Title"
-          name="title"
           ref={titleRef}
+          aria-label="Enter book title"
           className={`${variant === 'default' ? 'md:w-[48%]' : 'w-full'} ${style.input}`}
+          disabled={isLoading}
+          name="title"
+          placeholder="Title"
+          type="text"
         />
         <input
           aria-label="Enter book author"
-          type="text"
-          placeholder="Author"
-          name="author"
           className={`${variant === 'default' ? 'md:w-[30%]' : 'w-full'} ${style.input}`}
+          disabled={isLoading}
+          name="author"
+          placeholder="Author"
+          type="text"
         />
         <input
           aria-label="Enter optional rating (1-10, decimals welcome)"
-          type="number"
-          min="1"
-          max="10"
-          step="0.01"
-          placeholder="Rating"
-          name="rating"
           className={`${variant === 'default' ? 'md:w-[11%]' : 'w-full'} ${style.input}`}
+          disabled={isLoading}
+          max="10"
+          min="1"
+          name="rating"
+          placeholder="Rating"
+          step="0.01"
+          type="number"
         />
-        <button type="submit" title={'Add new book'} className={style.button}>
+        <button
+          className={`${style.button} disabled:cursor-not-allowed disabled:opacity-80`}
+          disabled={isLoading}
+          title={'Add new book'}
+          type="submit"
+        >
           <span className={`inline ${variant === 'default' && 'md:hidden'} `}>Add Book</span>
           {variant === 'default' && <span className="hidden md:inline">Add</span>}
         </button>
